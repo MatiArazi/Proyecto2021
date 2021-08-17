605cd73a3369fc21d8fc71db932f0e7392ff75f0
Matías Arazi <71347020+MatiArazi@users.noreply.github.com>
2021-08-17 19:25:41 +0000

    requirments

--- tesseractDemo.py
+++ tesseractDemo.py
@@ -14,5 +14,5 @@ for i in range(n_boxes):
         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-cv2.imshow('img', img)
+cv2.imshow('img', im)
 cv2.waitKey(0)
