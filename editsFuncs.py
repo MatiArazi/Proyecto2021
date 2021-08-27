96a903a9d1d451a6c9052ab4e803eb2c3fbeed72
MatiArazi <71347020+MatiArazi@users.noreply.github.com>
2021-08-27 11:34:53 -0300

    blur & pixelate

---
+++ editsFuncs.py
@@ -0,0 +1,21 @@
+import cv2
+
+def blur(imgFile, x, y, w, h, k):
+  #read image
+  img_src = cv2.imread(imgFile)
+  img_edt = img_src[y:y+h, x:x+w]
+  # applying a gaussian blur over this new rectangle area
+  img_edt = cv2.GaussianBlur(img_edt, (k, k), 0)
+  img_src[y:y+img_edt.shape[0], x:x+img_edt.shape[1]] = img_edt
+  return img_src
+
+def pixelate(imgFile, x, y, w, h, k):
+  #read image
+  img_src = cv2.imread(imgFile)
+  img_edt = img_src[y:y+h, x:x+w]
+  # Resize input to "pixelated" size
+  img_edt = cv2.resize(img_edt, (k, k), interpolation=cv2.INTER_LINEAR)
+  # Initialize output image
+  img_edt = cv2.resize(img_edt, (w, h), interpolation=cv2.INTER_NEAREST)
+  img_src[y:y+img_edt.shape[0], x:x+img_edt.shape[1]] = img_edt
+  return img_src
